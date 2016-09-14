fbfb9e24162a83550362e329372bd6fe651ed904
heidaertu <993409712@qq.com>
2016-09-14 21:35:42 +0900

    modify Hello World of master

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class HelloWorld {
 	public static void main(String [] args) {
+		System.out.println("for test...................");
 		System.out.println("Hello World!");
 	}
 
